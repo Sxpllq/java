a1f24f84ecc7ea2faf57a6c0d4f130b8ff77c99a
Sxpllq <mbluukacocucka@gmail.com>
2024-11-07 22:14:39 +0300

    StopWatch.java

--- lab9/src/StopWatch.java
+++ lab9/src/StopWatch.java
@@ -3,22 +3,18 @@ public class StopWatch {
     private long endTime;
 
     public StopWatch() {
-        // Конструктор, который инициализирует startTime
         startTime = System.currentTimeMillis();
     }
 
     public void start() {
-        // Метод для начала отсчета времени
         startTime = System.currentTimeMillis();
     }
 
     public void stop() {
-        // Метод для остановки отсчета времени
         endTime = System.currentTimeMillis();
     }
 
     public long getElapsedTime() {
-        // Метод для получения прошедшего времени
         return endTime - startTime;
     }
 }
